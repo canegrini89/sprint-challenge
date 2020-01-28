cefe3df15001bf09208e7585fae3cce5ee0e49e0
Ariel Negrini <canegrini89@gmail.com>
2020-01-28 15:48:45 -0300

    Calculator Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import './App.css';
 
 import Navbar from './components/Navbar';
 import Calculator from './components/Calculator';
--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,9 +1,181 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './Calculator.module.css';
 
 const Calculator = () => {
+  const [input, setInput] = useState('');
+
+  const handleClick = e => {
+    setInput(input + e.target.value);
+  };
+
+  const handleOperation = e => {
+    switch (e.target.value) {
+      case '+':
+        setInput(input + e.target.value);
+        break;
+      case '-':
+        setInput(input + e.target.value);
+        break;
+      case '*':
+        setInput(input + e.target.value);
+        break;
+      case '/':
+        setInput(input + e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleDone = () => {
+    setInput(eval(input));
+  };
+
+  const handleReset = () => {
+    setInput('');
+  };
+
+  console.log(input);
+
   return (
-    <div>
-      <h1>Calculator</h1>
+    <div className={styles.container}>
+      <div className={styles.calculator}>
+        <div className={styles.displayCont}>
+          <div className={styles.display}>{input}</div>
+        </div>
+
+        <div className={styles.buttonsCont}>
+          <div className={styles.buttonsLineCont}>
+            <button className={styles.upBtn} onClick={handleClick} value='('>
+              {'('}
+            </button>
+            <button className={styles.upBtn} onClick={handleClick} value=')'>
+              {')'}
+            </button>
+          </div>
+          <div className={styles.buttonsLineCont}>
+            <button
+              className={styles.normalBtn}
+              onClick={handleClick}
+              value='7'
+            >
+              7
+            </button>
+            <button
+              className={styles.normalBtn}
+              onClick={handleClick}
+              value='8'
+            >
+              8
+            </button>
+            <button
+              className={styles.normalBtn}
+              onClick={handleClick}
+              value='9'
+            >
+              9
+            </button>
+            <button
+              className={styles.anotherBtn}
+              value='/'
+              onClick={handleOperation}
+            >
+              /
+            </button>
+          </div>
+          <div className={styles.buttonsLineCont}>
+            <button
+              className={styles.normalBtn}
+              onClick={handleClick}
+              value='4'
+            >
+              4
+            </button>
+            <button
+              className={styles.normalBtn}
+              onClick={handleClick}
+              value='5'
+            >
+              5
+            </button>
+            <button
+              className={styles.normalBtn}
+              onClick={handleClick}
+              value='6'
+            >
+              6
+            </button>
+            <button
+              className={styles.anotherBtn}
+              onClick={handleOperation}
+              value='*'
+            >
+              *
+            </button>
+          </div>
+          <div className={styles.buttonsLineCont}>
+            <button
+              className={styles.normalBtn}
+              onClick={handleClick}
+              value='1'
+            >
+              1
+            </button>
+            <button
+              className={styles.normalBtn}
+              onClick={handleClick}
+              value='2'
+            >
+              2
+            </button>
+            <button
+              className={styles.normalBtn}
+              onClick={handleClick}
+              value='3'
+            >
+              3
+            </button>
+            <button
+              className={styles.anotherBtn}
+              onClick={handleOperation}
+              value='+'
+            >
+              +
+            </button>
+          </div>
+          <div className={styles.buttonsLineCont}>
+            <button
+              className={styles.normalBtn}
+              onClick={handleClick}
+              value='0'
+            >
+              0
+            </button>
+            <button
+              className={styles.normalBtn}
+              onClick={handleClick}
+              value='.'
+            >
+              .
+            </button>
+            <button className={styles.anotherBtn} onClick={handleReset}>
+              C
+            </button>
+            <button
+              className={styles.anotherBtn}
+              onClick={handleOperation}
+              value='-'
+            >
+              -
+            </button>
+          </div>
+          <div className={styles.buttonsLineCont}>
+            <button className={styles.iqualBtn} onClick={handleDone}>
+              =
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
